81c37c4fb25493f4a24d0605c15e1495bbc2f0a1
Raul Britto <raulbbritto@gmail.com>
2020-07-01 11:43:24 -0300

    Modifing the straight file and main file

--- Straight.py
+++ Straight.py
@@ -7,14 +7,23 @@ def straightFlop(flop, features):
     if Rank.difference(flop[0].rank, flop[1].rank) == 1 and Rank.difference(flop[1].rank, flop[2].rank) == 1:
         features[33] = 1
     # 1-gap flop
-    elif (Rank.difference(flop[0].rank, flop[1].rank) == 1 and Rank.difference(flop[0].rank, flop[2].rank) == 3) or (Rank.difference(flop[0].rank, flop[1].rank) == 2 and Rank.difference(flop[0].rank, flop[2].rank) == 3):
-        features[34] = 1
+    elif (Rank.difference(flop[0].rank, flop[1].rank) == 1 and Rank.difference(flop[0].rank, flop[2].rank) == 3) \
+        or (Rank.difference(flop[0].rank, flop[1].rank) == 2 and Rank.difference(flop[0].rank, flop[2].rank) == 3):
+            features[34] = 1
     # 2-gap flop
-    elif (Rank.difference(flop[0].rank, flop[1].rank) == 1 and Rank.difference(flop[0].rank, flop[2].rank) == 4) or (Rank.difference(flop[0].rank, flop[1].rank) == 3 and Rank.difference(flop[0].rank, flop[2].rank) == 4) or (Rank.difference(flop[0].rank, flop[1].rank) == 2 and Rank.difference(flop[0].rank, flop[2].rank) == 4) or (Rank.difference(flop[0].rank, flop[1].rank) == 0 and Rank.difference(flop[0].rank, flop[2].rank) == 3) or (Rank.difference(flop[1].rank, flop[2].rank) == 0 and Rank.difference(flop[0].rank, flop[1].rank) == 3):
-        features[35] = 1
+    elif (Rank.difference(flop[0].rank, flop[1].rank) == 1 and Rank.difference(flop[0].rank, flop[2].rank) == 4) \
+        or (Rank.difference(flop[0].rank, flop[1].rank) == 3 and Rank.difference(flop[0].rank, flop[2].rank) == 4) \
+        or (Rank.difference(flop[0].rank, flop[1].rank) == 2 and Rank.difference(flop[0].rank, flop[2].rank) == 4) \
+        or (Rank.difference(flop[0].rank, flop[1].rank) == 0 and Rank.difference(flop[0].rank, flop[2].rank) == 3) \
+        or (Rank.difference(flop[1].rank, flop[2].rank) == 0 and Rank.difference(flop[0].rank, flop[1].rank) == 3):
+            features[35] = 1
     # 3-gap flop
-    elif (Rank.difference(flop[0].rank, flop[1].rank) == 4 and Rank.difference(flop[0].rank, flop[2].rank) == 5) or (Rank.difference(flop[0].rank, flop[1].rank) == 1 and Rank.difference(flop[0].rank, flop[2].rank) == 5) or (Rank.difference(flop[0].rank, flop[1].rank) == 0 and Rank.difference(flop[0].rank, flop[2].rank) == 4) or (Rank.difference(flop[0].rank, flop[1].rank) == 0 and Rank.difference(flop[0].rank, flop[2].rank) == 4) or (Rank.difference(flop[1].rank, flop[2].rank) == 0 and Rank.difference(flop[0].rank, flop[1].rank) == 4):
-        features[36] = 1
+    elif (Rank.difference(flop[0].rank, flop[1].rank) == 4 and Rank.difference(flop[0].rank, flop[2].rank) == 5) \
+        or (Rank.difference(flop[0].rank, flop[1].rank) == 1 and Rank.difference(flop[0].rank, flop[2].rank) == 5) \
+        or (Rank.difference(flop[0].rank, flop[1].rank) == 0 and Rank.difference(flop[0].rank, flop[2].rank) == 4) \
+        or (Rank.difference(flop[0].rank, flop[1].rank) == 0 and Rank.difference(flop[0].rank, flop[2].rank) == 4) \
+        or (Rank.difference(flop[1].rank, flop[2].rank) == 0 and Rank.difference(flop[0].rank, flop[1].rank) == 4):
+            features[36] = 1
     # no straight or straight draw in the flop
     else:
         features[37] = 1
--- flopAnalyzer.py
+++ flopAnalyzer.py
@@ -5,6 +5,11 @@ import Broadway
 import Suits
 import Ranks
 import Straight
+import flushDraw
+import madeHands
+import Overcards
+import StraightDraw
+import DrawCombinations
 
 features = np.zeros(73, dtype=bool)
 
@@ -39,14 +44,48 @@ def sortFlop(flop):
     
     return sortedFlop
     
+def sortBoard(flop, hand):
+    board = flop.copy()
+    if hand.first > flop[0]:
+        board.insert(0, hand.first)
+    elif hand.first > flop[1]:
+        board.insert(1, hand.first)
+    elif hand.first > flop[2]:
+        board.insert(2, hand.first)
+    else:
+        board.append(hand.first)
+    
+    if hand.second > flop[0]:
+            board.insert(1, hand.second)
+    elif hand.second > flop[1]:
+        board.insert(2, hand.second)
+    elif hand.second > flop[2]:
+        board.insert(3, hand.second)
+    else:
+        board.append(hand.second)
+    
+    return board
 
 
+def FlopFeatures(hand, flop):
 
-def FlopFeatures(flop):
     sortedFlop = sortFlop(flop)
+    board = sortBoard(sortedFlop, hand)
+
+    # Features entirely on the flop
     Broadway.BroadwayCards(sortedFlop, features)
     Suits.SuitsCards(sortedFlop, features)
     Ranks.RankCards(sortedFlop, features)
     Straight.straightFlop(sortedFlop, features)
+    
+    # Features combining flop and hand
+    flushDraw.FlushDraws(hand , sortedFlop, board, features)
+    madeHands.checkMadeHands(hand, sortedFlop, board, features)
+    Overcards.CheckOvercards(hand, flop, features)
+    StraightDraw.StraightDraws(hand, sortedFlop, board, features)
+    DrawCombinations.CheckDrawCombinations(hand, sortedFlop, board, features)
 
     return features
+
+
+
